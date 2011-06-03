a39757617da7300fcd91f17924bd489eb4bc6043
daedrith <daedrith@gmail.com>
2011-06-03 12:45:14 -0400

    Changed the blending parameter to an enum, more changes to gui classes

---
+++ ThereBeMonsters/Back-end/Modules/InitMapsModule.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ThereBeMonsters.Back_end.Modules
+{
+  public class InitMapsModule : Module
+  {
+    [Parameter("Resolution of the terrain (# of verticies on one side)",
+      Default = 512)]
+    public int Size { private get; set; }
+    public byte[,] HeightMap { get; private set; }
+    public uint[,] ColorMap { get; private set; }
+
+    public override void Run()
+    {
+      HeightMap = new byte[Size, Size];
+      ColorMap = new uint[Size, Size];
+    }
+  }
+}
